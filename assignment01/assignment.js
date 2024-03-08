a4d35ffc5e29fd36ff29c90edbfa93f899843ea4
hasnainshafi <hasnainshafi45@gmail.com>
2024-03-08 16:53:16 +0500

    first commit

---
+++ assignment01/assignment.js
@@ -0,0 +1,98 @@
+// Question NO 1
+console.log('Hello World');
+// Question NO 2
+var c = ('Hello Eric, ');
+var f = ('would you like to learn some Python today ?');
+var g = (c + f);
+console.log(g);
+// Question NO 3
+var m = ('Eric');
+console.log(m.toLowerCase());
+console.log(m.toUpperCase());
+console.log(m.charAt(0).toUpperCase() + m.slice(1).toLowerCase());
+// Question NO 4
+var j = ('Albert Einstein once said, “A person who never made a mistake never tried anything new.”');
+console.log(j);
+// Question NO 5
+var famous_person = ('Quaid-e-Azam once said, ');
+var message = ('“I do not believe in taking the right decision, I take a decision and make it right.”');
+var out = (famous_person + message);
+console.log(out);
+// Question NO 6
+var o = ('Eric');
+console.log("\t" + o + "\t");
+console.log("\n" + o + "\n");
+console.log("\n" + o + "\t");
+console.log("\t" + o + "\n");
+// Question NO 7 & 8
+console.log("Method 1 with Variable");
+var p = (4 + 4);
+var q = (12 - 4);
+var u = (4 * 2);
+var i = (24 / 3);
+console.log('Addition = ' + p);
+console.log('Subtraction = ' + q);
+console.log('Multiplication = ' + u);
+console.log('Division = ' + i + '\n');
+console.log("Method 2 without variable");
+console.log('Addition = ' + (6 + 2));
+console.log('Subtraction = ' + (20 - 12));
+console.log('Multiplication = ' + (4 * 2));
+console.log('Division = ' + (24 / 3));
+// Question NO 9
+var favoritenumber = 1000;
+var msg = ('My favorite number is ');
+var mn = (msg + favoritenumber);
+console.log(mn);
+// Question NO 10
+// let favoritenumber = 1000;
+// let msg = ('My favorite number is ')
+// let mn = (msg+favoritenumber)
+// console.log(mn);
+// Question NO 10
+// let favoritenumber = 1000;
+// let msg = ('My favorite number is ')
+// let mn = (msg+favoritenumber)
+// console.log(mn);
+//Question NO 11
+console.log("Method No 1 without for loop");
+var names = ["Ali", "Danish", "Hanif"];
+console.log(names);
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2] + '\n');
+console.log("Method No 2 with for loop");
+var nam = ["Akram", "Kamran", "Basit", "Muqeet"];
+for (var i_1 = 0; i_1 < 4; i_1++) {
+    console.log(nam[i_1]);
+}
+//Question NO 12
+console.log("Method No 1 with personalized message");
+var nms = ["Ali", "Danish", "Hanif", "Karim"];
+console.log('Hello ' + nms[0]);
+console.log('How are you ' + nms[1] + ' ?');
+console.log(nms[2] + ' what is your father name ?');
+console.log('Have a nice day ' + nms[3] + '\n');
+console.log("Method No 2 without personalized message");
+var nm = ["Akram", "Kamran", "Basit", "Muqeet"];
+for (var i_2 = 0; i_2 < 4; i_2++) {
+    console.log('He is my best friend ' + nm[i_2]);
+}
+//Question NO 13
+console.log("Method No 1 with motorbikes message");
+var motors = ["Honda", "Supreme", "Suzuki", "Crown"];
+console.log('I would like to own a ' + motors[0] + ' motorcycle');
+console.log('I would like to race on a ' + motors[1] + ' motorcycle');
+console.log('I would like to sit on a ' + motors[2] + ' motorcycle');
+console.log('I would like to spend a whole day on a ' + motors[3] + ' motorcycle' + '\n');
+console.log("Method No 1 with motorbikes message");
+var motrs = ["Honda", "Supreme", "Suzuki", "Crown"];
+console.log('I would like to own a ' + motrs[0] + ' motorcycle');
+console.log('I would like to race on a ' + motrs[1] + ' motorcycle');
+console.log('I would like to sit on a ' + motrs[2] + ' motorcycle');
+console.log('I would like to spend a whole day on a ' + motrs[3] + ' motorcycle' + '\n');
+// Question NO 14
+var guest = ["Ahmed", "Danish", "Saleem"];
+console.log(guest[0] + ' you are invited for dinner at Governor House');
+console.log(guest[1] + ' you are invited for dinner at Governor House');
+console.log(guest[2] + ' you are invited for dinner at Governor House');
---
+++ assignment01/assignment.ts
@@ -0,0 +1,116 @@
+// Question NO 1
+console.log('Hello World')
+
+// Question NO 2
+let c =('Hello Eric, ')
+let f =('would you like to learn some Python today ?')
+let g =(c+f)
+console.log(g)
+
+// Question NO 3
+let m =('Eric')
+console.log(m.toLowerCase())
+console.log(m.toUpperCase())
+console.log(m.charAt(0).toUpperCase() + m.slice(1).toLowerCase())
+
+// Question NO 4
+let j = ('Albert Einstein once said, “A person who never made a mistake never tried anything new.”')
+console.log(j)
+
+// Question NO 5
+let famous_person =('Quaid-e-Azam once said, ')
+let message = ('“I do not believe in taking the right decision, I take a decision and make it right.”')
+let out = (famous_person + message)
+console.log(out)
+
+// Question NO 6
+let o =('Eric')
+console.log("\t"+o+"\t")
+console.log("\n"+o+"\n")
+console.log("\n"+o+"\t")
+console.log("\t"+o+"\n")
+
+// Question NO 7 & 8
+console.log("Method 1 with Variable")
+let p =(4+4)
+let q =(12-4)
+let u =(4*2)
+let i =(24/3)
+console.log('Addition = '+p)
+console.log('Subtraction = '+q)
+console.log('Multiplication = '+u)
+console.log('Division = '+i +'\n')
+
+console.log("Method 2 without variable")
+console.log('Addition = ' +(6+2))
+console.log('Subtraction = '+(20-12) )
+console.log('Multiplication = '+(4*2))
+console.log('Division = ' +(24/3))
+
+// Question NO 9
+let favoritenumber = 1000;
+let msg = ('My favorite number is ')
+let mn = (msg+favoritenumber)
+console.log(mn);
+
+// Question NO 10
+// let favoritenumber = 1000;
+// let msg = ('My favorite number is ')
+// let mn = (msg+favoritenumber)
+// console.log(mn);
+
+// Question NO 10
+// let favoritenumber = 1000;
+// let msg = ('My favorite number is ')
+// let mn = (msg+favoritenumber)
+// console.log(mn);
+
+//Question NO 11
+console.log("Method No 1 without for loop")
+let names = ["Ali", "Danish", "Hanif"];
+console.log(names)
+console.log(names[0])
+console.log(names[1])
+console.log(names[2] +'\n')
+
+console.log("Method No 2 with for loop")
+let nam = ["Akram", "Kamran", "Basit", "Muqeet"];
+for (let i = 0; i < 4; i++) {
+    console.log(nam[i]);
+}
+
+//Question NO 12
+console.log("Method No 1 with personalized message")
+let nms = ["Ali", "Danish", "Hanif", "Karim"];
+console.log('Hello '+nms[0])
+console.log('How are you '+nms[1]+' ?')
+console.log(nms[2]+' what is your father name ?')
+console.log('Have a nice day '+nms[3] +'\n')
+
+console.log("Method No 2 without personalized message")
+let nm = ["Akram", "Kamran", "Basit", "Muqeet"];
+for (let i = 0; i < 4; i++) {
+    console.log('He is my best friend '+nm[i]);
+}
+
+//Question NO 13
+console.log("Method No 1 with motorbikes message")
+let motors = ["Honda", "Supreme", "Suzuki", "Crown"];
+console.log('I would like to own a '+motors[0]+' motorcycle')
+console.log('I would like to race on a '+motors[1]+' motorcycle')
+console.log('I would like to sit on a '+motors[2]+' motorcycle')
+console.log('I would like to spend a whole day on a '+motors[3]+' motorcycle' +'\n')
+
+
+console.log("Method No 1 with motorbikes message")
+let motrs = ["Honda", "Supreme", "Suzuki", "Crown"];
+console.log('I would like to own a '+motrs[0]+' motorcycle')
+console.log('I would like to race on a '+motrs[1]+' motorcycle')
+console.log('I would like to sit on a '+motrs[2]+' motorcycle')
+console.log('I would like to spend a whole day on a '+motrs[3]+' motorcycle' +'\n')
+
+// Question NO 14
+let guest = ["Ahmed", "Danish", "Saleem"];
+console.log(guest[0]+' you are invited for dinner at Governor House')
+console.log(guest[1]+' you are invited for dinner at Governor House')
+console.log(guest[2]+' you are invited for dinner at Governor House')
